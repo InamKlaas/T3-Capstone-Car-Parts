eefac0bfbccd69b5ac1398e8532968e5784fa617
Onika <231089945@mycput.ac.za>
2026-07-27 11:07:41 +0200

    updated

--- src/main/java/za/ac/cput/carpartmarket/Factory/CarPartFactory.java
+++ src/main/java/za/ac/cput/carpartmarket/Factory/CarPartFactory.java
@@ -5,22 +5,9 @@ import za.ac.cput.carpartmarket.Util.Helper;
 
 public class CarPartFactory {
 
-    public static CarPart createCarPart(String partName,
-                                        String description,
-                                        String model,
-                                        Long sellerId) {
+    public static CarPart createCarPart(String partName, String description, String model, Long sellerId) {
 
-        if (Helper.isNullOrEmpty(partName))
-            return null;
-
-        if (Helper.isNullOrEmpty(description))
-            return null;
-
-        if (Helper.isNullOrEmpty(model))
-            return null;
-
-
-        if (Helper.isEmptyOrNull(sellerId))
+        if (Helper.isNullOrEmpty(partName) || Helper.isNullOrEmpty(description) || Helper.isNullOrEmpty(model) || Helper.isEmptyOrNull(sellerId))
             return null;
 
         return new CarPart.Builder()
