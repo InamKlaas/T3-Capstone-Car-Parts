85f83fb001a8ca812ce02db0183a542e71a7d009
Lisakhanya Jonga <lisakhanyatinaye@gmail.com>
2026-08-22 23:02:19 +0200

    Login

---
+++ src/main/java/za/ac/cput/carpartmarket/Service/LoginService.java
@@ -0,0 +1,34 @@
+package za.ac.cput.carpartmarket.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.carpartmarket.Domain.Login;
+import za.ac.cput.carpartmarket.Repository.ILoginRepository;
+
+@Service
+public class LoginService implements ILoginService {
+
+    @Autowired
+    private ILoginRepository repository;
+
+    @Override
+    public Login create(Login login) {
+        return repository.save(login);
+    }
+
+    @Override
+    public Login read(Long loginId) {
+        return repository.findById(loginId).orElse(null);
+    }
+
+    @Override
+    public Login update(Login login) {
+        return repository.save(login);
+    }
+
+    @Override
+    public boolean delete(Long loginId) {
+        repository.deleteById(loginId);
+        return true;
+    }
+}
